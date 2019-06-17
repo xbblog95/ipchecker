5a9c932ea12fa3082d9b1d73b30ca15ad8425c30
xbblog <f550529870@gmail.com>
2019-06-17 18:18:20 +0800

    不允许私有ip

--- src/main/java/com/xbblog/ipchecker/controller/CheckController.java
+++ src/main/java/com/xbblog/ipchecker/controller/CheckController.java
@@ -16,19 +16,40 @@ public class CheckController {
 
     @Autowired
     private CheckService checkService;
+
     @RequestMapping("api_v1")
     @ResponseBody
-    public ResultVo testTcpActive(String host, int port)
-    {
+    public ResultVo testTcpActive(String host, String port) {
         ResultVo resultVo = new ResultVo();
-        if(StringUtils.isEmpty(host))
-        {
+        if (StringUtils.isEmpty(host)) {
             resultVo.setSuccess(false);
             resultVo.setMsg("host and port is must");
             return resultVo;
         }
-        long time = checkService.testTcpActive(host, port);
-
+        if (StringUtils.isEmpty(port)) {
+            resultVo.setSuccess(false);
+            resultVo.setMsg("host and port is must");
+            return resultVo;
+        }
+        String rexp = "([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}";
+        //如果是ip
+        if (host.matches(rexp))
+        {
+            if (isprivateIp(host))
+            {
+                resultVo.setSuccess(false);
+                resultVo.setMsg(host + " is a priavte IP");
+                return resultVo;
+            }
+        }
+        try {
+            Integer.parseInt(port);
+        } catch (Exception e) {
+            resultVo.setSuccess(false);
+            resultVo.setMsg("port must be a number");
+            return resultVo;
+        }
+        long time = checkService.testTcpActive(host, Integer.parseInt(port));
         ResultDataVo resultDataVo = new ResultDataVo();
         ResultInsideVo resultInsideVo = new ResultInsideVo();
         resultInsideVo.setTcp(time != 0);
@@ -38,4 +59,37 @@ public class CheckController {
         resultVo.setSuccess(time != 0);
         return resultVo;
     }
+
+    public static boolean isprivateIp(String ip) {
+        long ipNum = getIpNum(ip);
+        long aBegin = getIpNum("10.0.0.0");
+        long aEnd = getIpNum("10.255.255.255");
+        long bBegin = getIpNum("172.16.0.0");
+        long bEnd = getIpNum("172.31.255.255");
+        long cBegin = getIpNum("192.168.0.0");
+        long cEnd = getIpNum("192.168.255.255");
+        long ringBegin = getIpNum("127.0.0.0");
+        long ringEnd = getIpNum("127.255.255.255");
+        boolean isInnerIp = isInner(ipNum, aBegin, aEnd) || isInner(ipNum, bBegin, bEnd)
+                || isInner(ipNum, cBegin, cEnd) || isInner(ipNum, ringBegin, ringEnd);
+        return isInnerIp;
+    }
+    private static long getIpNum(String ip) {
+        String[] ipArray = ip.split("\\.");
+        long a = Integer.parseInt(ipArray[0]);
+        long b = Integer.parseInt(ipArray[1]);
+        long c = Integer.parseInt(ipArray[2]);
+        long d = Integer.parseInt(ipArray[3]);
+        long ipNum = a * 256 * 256 * 256 + b * 256 * 256 + c * 256 + d;
+        return ipNum;
+    }
+
+    /**
+     *  判断IP转化后的数字是否是区间内
+     *     
+     */
+    private static boolean isInner(long ipNum, long begin, long end) {
+        return (ipNum >= begin) && (ipNum <= end);
+    }
+
 }
