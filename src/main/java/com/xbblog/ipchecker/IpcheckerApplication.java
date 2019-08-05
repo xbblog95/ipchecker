9bbbb8e4279a3644b453f24c7fe0c048bac17281
xbblog <f550529870@gmail.com>
2019-08-05 09:48:27 +0800

    简化启动类

--- src/main/java/com/xbblog/ipchecker/IpcheckerApplication.java
+++ src/main/java/com/xbblog/ipchecker/IpcheckerApplication.java
@@ -2,20 +2,11 @@ package com.xbblog.ipchecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @SpringBootApplication
 public class IpcheckerApplication {
 
-    @RequestMapping("/")
-    public void home(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.html").forward(request, response);
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(IpcheckerApplication.class, args);
     }
